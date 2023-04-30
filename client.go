b6522a292b05b0c9254b18cfec2c74e832943685
Matthias Mayer <8875306+mfmayer@users.noreply.github.com>
2023-04-30 13:03:34 +0200

    added initial functions and tests

--- client.go
+++ client.go
@@ -1,5 +1,12 @@
 package oai
 
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
 type Client struct {
 	openaiApiKey string
 }
@@ -8,4 +15,36 @@ func NewClient(openaiApKey string) *Client {
 	return &Client{
 		openaiApiKey: openaiApKey,
 	}
+}
+
+func (c *Client) GetChatCompletion(prompt *ChatPrompt) (completion *ChatCompletion, err error) {
+	url := "https://api.openai.com/v1/chat/completions"
+	headers := map[string]string{
+		"Content-Type":  "application/json",
+		"Authorization": "Bearer " + c.openaiApiKey,
+	}
+	jsonData, err := json.Marshal(prompt)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	completion = &ChatCompletion{}
+	err = json.Unmarshal(body, completion)
+	if err != nil {
+		return nil, err
+	}
+	return
 }
---
+++ client_test.go
@@ -0,0 +1,23 @@
+package oai
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+	key := os.Getenv("OPENAI_API_KEY")
+
+	c := NewClient(key)
+	cC, err := c.GetChatCompletion(&ChatPrompt{
+		Model: "gpt-3.5-turbo",
+		Messages: []*Message{
+			{Role: "user", Content: "Hallo"},
+		},
+	})
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	fmt.Println(cC)
+}
